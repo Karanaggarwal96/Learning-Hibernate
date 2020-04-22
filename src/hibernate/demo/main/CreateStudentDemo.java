6af2ea89575a4b1caae510f71ebb751cba2a40fb
Karanaggarwal96 <karanaggarwal30@gmail.com>
2020-04-22 12:10:32 -0400

    Working with Hibernate Annotations One to One

--- src/hibernate/demo/main/CreateStudentDemo.java
+++ src/hibernate/demo/main/CreateStudentDemo.java
@@ -6,32 +6,32 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
+import hibernate.demo.entity.Instructor;
+import hibernate.demo.entity.InstructorDetail;
 import hibernate.demo.entity.student;
 
 public class CreateStudentDemo {
 
 	public static void main(String[] args) {
 
-		SessionFactory factory=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(student.class).buildSessionFactory();
+		SessionFactory factory=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
 			
 		
 		//create session
 		Session session=factory.getCurrentSession();
 		
 		try {
-			int studentId=5;
-		
-			session.beginTransaction();
-		    student  myStudent=session.get(student.class, studentId);	
-		    session.delete(myStudent);
-			session.getTransaction().commit(); 
 			
-			Session session1=factory.getCurrentSession();
-			session1.beginTransaction();
+			session.beginTransaction();
 		    
-			session1.createQuery("delete from student where id=2").executeUpdate();
-			session1.getTransaction().commit(); 
+			Instructor tempInstructor=new Instructor("Karan","Aggarwal","karanaggarwal30@gmail.com");
+			
+			InstructorDetail tempDetail=new  InstructorDetail("http://www.tech4techies.com","Coding");
+			
+			tempInstructor.setInstructorDetail(tempDetail);
+			
+			session.save(tempInstructor);
+			session.getTransaction().commit(); 
 			
 			
 			
